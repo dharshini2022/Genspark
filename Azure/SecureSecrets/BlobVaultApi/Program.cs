70ada8d8dafd0ec1b6d3f6ad6ec2ae98264f309e
Dharshini Karthik <dharshinikarthik06@gmail.com>
2026-07-01 22:51:34 +0530

    Azure - GUI & CLI

---
+++ Azure/SecureSecrets/BlobVaultApi/Program.cs
@@ -0,0 +1,23 @@
+using Azure.Identity;
+using Azure.Storage.Blobs;
+ 
+var builder = WebApplication.CreateBuilder(args);
+ 
+var keyVaultUri = new Uri($"https://{builder.Configuration["KeyVaultName"]}.vault.azure.net/");
+builder.Configuration.AddAzureKeyVault(keyVaultUri, new DefaultAzureCredential());
+ 
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton(x =>
+{
+    var connStr = builder.Configuration["BlobStorageConnectionString"];
+    return new BlobServiceClient(connStr);
+});
+
+var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
