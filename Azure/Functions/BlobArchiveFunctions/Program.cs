73ad7200b211c7864002af27b958d1adc018207d
Dharshini Karthik <dharshinikarthik06@gmail.com>
2026-07-02 22:24:09 +0530

    Azure Functions

---
+++ Azure/Functions/BlobArchiveFunctions/Program.cs
@@ -0,0 +1,15 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using BlobArchiveFunctions;
+ 
+var host = new HostBuilder()
+    .ConfigureFunctionsWebApplication()
+    .ConfigureServices(services =>
+    {
+        services.AddTransient<BlobArchiveService>();
+    })
+    .Build();
+ 
+host.Run();
+
